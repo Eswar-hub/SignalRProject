9a8ea57bd0001695f5fb2f85d2037ed641737438
Dongala Eswar <eswar.d@compugain.com>
2021-02-16 19:26:53 +0530

    signalRconsoleClient

---
+++ SignalRConsoleClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace SignalRConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder().WithUrl("http://localhost:5000/notification").Build();
+            connection.StartAsync().Wait();
+            connection.InvokeAsync("SendMessage", "eswar", "Hello World");
+            connection.On("ReceiveMessage", (string username, string message) =>
+             {
+                 Console.WriteLine(username + ":" + message);
+             });
+            Console.ReadLine();
+        }
+    }
+}
